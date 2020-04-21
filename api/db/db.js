878b1da1af6010ae5cee83d64e3519617e144bbc
Ashim11 <ashimgurung433@gmail.com>
2020-04-21 19:31:18 +0000

    Moved routes to api/routes, add initial connection to local db

---
+++ api/db/db.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/cars', { useNewUrlParser: true});
+var connection = mongoose.connection;
+
+if (!connection) {
+  console.log('Error connecting db');
+}
+else {
+  console.log('Connected');
+}
+
+module.exports = connection;
